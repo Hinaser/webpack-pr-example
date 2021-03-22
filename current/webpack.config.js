82d58717b30e122965b16cc47bb95c4178f564fd
Hinaser <rindo.hinase@gmail.com>
2021-03-23 01:15:22 +0900

    Added docs/

--- current/webpack.config.js
+++ current/webpack.config.js
@@ -1,16 +1,32 @@
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: {
-    moduleA: "./src/moduleA.js",
-    moduleB: "./src/moduleB.js",
+module.exports = [
+  {
+    mode: "development",
+    entry: {
+      moduleA: "./src/moduleA.js",
+      moduleB: "./src/moduleB.js",
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].js",
+      library: ["MyLibrary"],
+      libraryTarget: "umd",
+      globalObject: "this",
+    },
   },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
-    library: ["MyLibrary"],
-    libraryTarget: "umd",
-    globalObject: "this",
-  },
-};
+  {
+    mode: "development",
+    entry: {
+      moduleA: "./src/moduleA.js",
+      moduleB: "./src/moduleB.js",
+    },
+    output: {
+      path: path.resolve(__dirname, "..", "docs", "current"),
+      filename: "[name].js",
+      library: ["MyLibrary"],
+      libraryTarget: "umd",
+      globalObject: "this",
+    },
+  }
+];
