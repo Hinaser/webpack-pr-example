d391e18487229a8af6db8ee7392ce79eaec3a30f
Hinaser <rindo.hinase@gmail.com>
2021-03-23 00:58:02 +0900

    Created sample for webpack PR

---
+++ proposing/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    moduleA: "./src/moduleA.js",
+    moduleB: "./src/moduleB.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    library: ["MyLibrary"],
+    libraryTarget: "umd",
+    globalObject: "this",
+  },
+};
